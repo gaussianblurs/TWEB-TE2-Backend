8140e4e09d4ab5858683f576ebd5fdcde7068213
GaussianBlurs <arnoldvbg@gmail.com>
2019-01-10 12:02:16 +0100

    Preparing for deployment

--- conf.js
+++ conf.js
@@ -1,7 +1,7 @@
 const conf = {
 	db: {
 		port: "27017",
-		host: process.env.NODE_ENV === "production" ? process.env.MONGODB_ENDPOINT_URL : "mongodb",
+		host: "mongodb",
 		name: "te2tweb",
 		usr: "dev",
 		pwd: "dev",
@@ -11,6 +11,6 @@ const conf = {
 	secret: "undyingandeverlastingfurnaces"
 }
 
-conf.db.url = `mongodb://${conf.db.usr}:${conf.db.pwd}@${conf.db.host}:${conf.db.port}/${conf.db.name}`
+conf.db.url = process.env.NODE_ENV === "production" ? `mongodb://dev:dev@tweb-te2-shard-00-00-rl5i0.mongodb.net:27017,tweb-te2-shard-00-01-rl5i0.mongodb.net:27017,tweb-te2-shard-00-02-rl5i0.mongodb.net:27017/test?ssl=true&replicaSet=TWEB-TE2-shard-0&authSource=admin&retryWrites=true` : `mongodb://${conf.db.usr}:${conf.db.pwd}@${conf.db.host}:${conf.db.port}/${conf.db.name}`
 
 exports.default = conf
